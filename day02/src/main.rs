3b1c0f36a3fa79852553df644e9e00e18434fbb9
misclicl <raddishman@protonmail.com>
2023-04-29 18:24:00 +0300

    Solve part 2 of day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -10,16 +10,24 @@ impl FromStr for Movement {
     type Err = String;
 
     fn from_str(input: &str) -> Result<Movement, Self::Err> {
-        let entries: Vec<&str> = input.split_whitespace().collect();
+        let mut entries = input.split_whitespace();
 
-        if entries.len() != 2 {
-            todo!();
+        let direction = match entries.next() {
+            Some(direction) => direction,
+            None => return Err("Failed to parse direction".to_string()),
+        };
+
+        let value = match entries.next() {
+            Some(value_str) => match value_str.parse::<i32>() {
+                Ok(value) => value,
+                Err(_) => return Err("Failed to parse value".to_string()),
+            },
+            None => return Err("Couldn't parse value".to_string()),
+        };
+
+        if entries.next().is_some() {
+            return Err("Too many arguments".to_string());
         }
-
-        let direction = entries[0];
-        let value = entries[1]
-            .parse::<i32>()
-            .expect("Failed to parse intruction value");
 
         match direction {
             "forward" => Ok(Movement::Horizontal(value)),
@@ -30,8 +38,8 @@ impl FromStr for Movement {
     }
 }
 
-fn part1() -> Result<i32, String> {
-    let course_instructions: Vec<Movement> = include_str!("data.txt")
+fn part1(data: &str) -> Result<i32, String> {
+    let course_instructions: Vec<Movement> = data
         .lines()
         .map(|line| Movement::from_str(line).unwrap())
         .collect();
@@ -49,7 +57,42 @@ fn part1() -> Result<i32, String> {
     Ok(pos_x * pos_y)
 }
 
+fn part2(data: &str) -> Result<i32, String> {
+    let course_instructions: Vec<Movement> = data
+        .lines()
+        .map(|line| Movement::from_str(line).unwrap())
+        .collect();
+
+    let (pos_x, pos_y, aim) =
+        course_instructions
+            .iter()
+            .fold((0, 0, 0), |(pos_x, pos_y, aim), m| match m {
+                Movement::Horizontal(value) => (pos_x + value, pos_y + value * aim, aim),
+                Movement::Vertical(value) => (pos_x, pos_y, aim + value),
+            });
+
+    Ok(pos_x * pos_y)
+}
+
 fn main() {
-    let result1 = part1().expect("Failed to calculate result for part 1");
+    let data = include_str!("data.txt");
+    let result1 = part1(data).expect("Failed to calculate result for part 1");
     println!("result #1: {}", result1);
+    let result2 = part2(data).expect("Failed to calculate result for part 1");
+    println!("result #2: {}", result2);
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn part1() {
+        let result = crate::part1(include_str!("data_small.txt")).unwrap();
+        assert_eq!(result, 150);
+    }
+
+    #[test]
+    fn part2() {
+        let result = crate::part2(include_str!("data_small.txt")).unwrap();
+        assert_eq!(result, 900);
+    }
 }

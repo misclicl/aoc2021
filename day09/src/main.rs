5bcc8478a792d8b410dc3a44cc62b9b7279a263a
misclicl <raddishman@protonmail.com>
2023-05-06 01:15:04 +0300

    Solve part 2 of day09

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -12,10 +12,13 @@ struct HeightMap {
 impl HeightMap {
     const NEIGHBOR_INDICES: [(i32, i32); 4] = [(-1, 0), (0, 1), (0, -1), (1, 0)];
 
+    fn size(map: &Map) -> (usize, usize) {
+        (map.len(), map[0].len())
+    }
+
     fn is_low_point(map: &Map, row_idx: usize, col_idx: usize) -> bool {
         let current_value = map[row_idx][col_idx];
-        let map_height = map.len() as i32;
-        let map_width = map[0].len() as i32;
+        let (map_height, map_width) = Self::size(map);
 
         Self::NEIGHBOR_INDICES
             .iter()
@@ -25,8 +28,8 @@ impl HeightMap {
 
                 if neighbor_row < 0
                     || neighbor_col < 0
-                    || neighbor_row >= map_height
-                    || neighbor_col >= map_width
+                    || neighbor_row >= map_height as i32
+                    || neighbor_col >= map_width as i32
                 {
                     return true;
                 }
@@ -53,11 +56,54 @@ impl HeightMap {
         }
     }
 
+    fn calc_basin_area(&self, low_point: MapCoords) -> u32 {
+        let (map_height, map_width) = Self::size(&self.map);
+
+        let mut stack: Vec<MapCoords> = vec![low_point];
+        let mut visited: HashSet<MapCoords> = HashSet::new();
+
+        while !stack.is_empty() {
+            let curr_coords = stack.pop().unwrap();
+            let (row_idx, col_idx) = curr_coords;
+
+            visited.insert(curr_coords);
+
+            Self::NEIGHBOR_INDICES
+                .iter()
+                .for_each(|&(row_offset, col_offset)| {
+                    let neighbor_row = row_idx as i32 + row_offset;
+                    let neighbor_col = col_idx as i32 + col_offset;
+
+                    if neighbor_row >= 0
+                        && neighbor_col >= 0
+                        && neighbor_row < map_height as i32
+                        && neighbor_col < map_width as i32
+                        && !visited.contains(&(neighbor_row as usize, neighbor_col as usize))
+                        && self.map[neighbor_row as usize][neighbor_col as usize] != 9
+                    {
+                        stack.push((neighbor_row as usize, neighbor_col as usize));
+                    }
+                })
+        }
+        visited.len() as u32
+    }
+
     fn calc_risk_level(&self) -> u32 {
         self.low_points.iter().fold(0, |acc, (y, x)| {
             let value = &self.map[*y][*x];
             acc + *value + 1
         })
+    }
+
+    fn calc_basins_risk_level(&self) -> u32 {
+        let mut sorted = self
+            .low_points
+            .iter()
+            .map(|(y, x)| self.calc_basin_area((*y, *x)))
+            .collect::<Vec<_>>();
+
+        sorted.sort_by(|a, b| b.cmp(a));
+        sorted[..3].iter().product()
     }
 }
 
@@ -74,14 +120,21 @@ fn part1(data: &str) -> u32 {
     height_map.calc_risk_level()
 }
 
+fn part2(data: &str) -> u32 {
+    let map_data = parse_input(data);
+
+    let height_map = HeightMap::new(map_data);
+    height_map.calc_basins_risk_level()
+}
+
 fn main() {
     let data = include_str!("data.txt");
     let result = part1(data);
     println!("result#1: {}", result);
 
-    // let data = include_str!("data.txt");
-    // let result = part2(data);
-    // println!("result#2: {}", result);
+    let data = include_str!("data_example.txt");
+    let result = part2(data);
+    println!("result#2: {}", result);
 }
 
 #[cfg(test)]
@@ -100,5 +153,19 @@ mod test {
         let data = include_str!("data.txt");
         let result = part1(data);
         assert_eq!(result, 496);
+    }
+
+    #[test]
+    fn part2_example() {
+        let data = include_str!("data_example.txt");
+        let result = part2(data);
+        assert_eq!(result, 1134);
+    }
+
+    #[test]
+    fn part2_large() {
+        let data = include_str!("data.txt");
+        let result = part2(data);
+        assert_eq!(result, 902880);
     }
 }
